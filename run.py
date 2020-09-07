02af81d4e8bfd7977901429f0237336981153f84
morch <70644384+morch1@users.noreply.github.com>
2020-09-07 21:02:06 +0200

    find_common_songs script

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 import argparse
 import spotipy
 import spotipy.util as util
-from scripts import new_releases, on_repeat, on_this_day
+from scripts import new_releases, on_repeat, on_this_day, find_common_songs
 
 SCOPE = 'playlist-read-private playlist-read-collaborative user-library-read playlist-modify-private playlist-modify-public user-follow-read'
 
@@ -26,6 +26,12 @@ def main():
     on_this_day_parser.add_argument('lastfm_username')
     on_this_day_parser.add_argument('playlist_ids', nargs='+')
 
+    find_common_songs_parser = subparsers.add_parser('find_common_songs')
+    find_common_songs_parser.add_argument('playlist1')
+    find_common_songs_parser.add_argument('playlist2')
+    find_common_songs_parser.add_argument('--by_name', action='store_true')
+    find_common_songs_parser.add_argument('--find_missing', action='store_true')
+
     args = parser.parse_args()
     
     if args.dotenv:
@@ -40,6 +46,8 @@ def main():
         on_repeat.update(token, args.on_repeat_id, args.playlist_id)
     elif args.command == 'on_this_day':
         on_this_day.update(token, args.lastfm_username, args.playlist_ids)
+    elif args.command == 'find_common_songs':
+        find_common_songs.run(token, args.username, args.playlist1, args.playlist2, args.by_name, args.find_missing)
 
 
 if __name__ == '__main__':
---
+++ scripts/find_common_songs.py
@@ -0,0 +1,37 @@
+import os
+import spotipy
+from datetime import datetime
+
+
+def run(token, username, playlist1, playlist2, by_name, find_missing):
+    sp = spotipy.Spotify(auth=token)
+
+    tracks1 = {}
+    common_tracks = []
+
+    print('getting tracks from playlist 1...')
+
+    songs = sp.playlist(playlist1)['tracks']
+    while songs:
+        for song in songs['items']:
+            tracks1[song['track']['id']] = (song['track']['artists'][0]['id'], song['track']['name'])
+        songs = sp.next(songs) if songs['next'] else None
+
+    print('getting tracks from playlist 2 and comparing...')
+
+    songs = sp.playlist(playlist2)['tracks']
+    while songs:
+        for song in songs['items']:
+            on_both = song['track']['id'] in tracks1 or (by_name and any(s1[0] == song['track']['artists'][0]['id'] and s1[1] == song['track']['name'] for _, s1 in tracks1.items()))
+            if (not find_missing and on_both) or (find_missing and not on_both):
+                common_tracks.append(song['track']['id'])
+        songs = sp.next(songs) if songs['next'] else None
+
+    print('saving result...')
+
+    if len(common_tracks) > 0:
+        result_playlist = sp.user_playlist_create(username, f'find_common_songs result {datetime.now().strftime("%Y-%m-%d %H:%M:%S")}')
+        for i in range(0, len(common_tracks) // 100 + 1):
+            sp.playlist_add_items(result_playlist['id'], common_tracks[i * 100 : (i + 1) * 100])
+    
+    print('found', len(common_tracks))
