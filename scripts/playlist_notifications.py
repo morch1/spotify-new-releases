da43a0c1950ed3ab035a6276a37b4b06e797e381
morch <70644384+morch1@users.noreply.github.com>
2020-09-19 02:49:25 +0200

    h

--- scripts/playlist_notifications.py
+++ scripts/playlist_notifications.py
@@ -32,6 +32,7 @@ def run(token, dry, device_ids, config_path, db_path, **_):
         print(f'checking playlist {playlist["name"]}')
         songs = playlist['tracks']
         new_playlist = playlist['id'] not in db
+        current_tracks = []
         if new_playlist:
             db[playlist['id']] = []
         else:
@@ -42,14 +43,15 @@ def run(token, dry, device_ids, config_path, db_path, **_):
                 if not new_playlist and song['track']['id'] not in db[playlist['id']] and song['added_by']['id'] != username:
                     new_songs += 1
                     added_by.add(song['added_by']['id'])
-                db[playlist['id']].append(song['track']['id'])
+                current_tracks.append(song['track']['id'])
             songs = sp.next(songs) if songs['next'] else None
+        db[playlist['id']] = current_tracks
         if not new_playlist and new_songs > 0:
             added_by = [sp.user(uid)['display_name'] for uid in added_by]
-            print(f'{new_songs} track(s) added to {playlist["name"]} by {", ".join(added_by)}')
             url = f'https://joinjoaomgcd.appspot.com/_ah/api/messaging/v1/sendPush?deviceIds={device_ids}&apikey={os.getenv("JOIN_API_KEY")}' + \
                 f'&url=https%3A%2F%2Fopen.spotify.com%2Fplaylist%2F{playlist["id"]}&group=Spotify playlist updates' + \
-                f'&title={playlist["name"]} updated&text={new_songs} track(s) added by {", ".join(added_by)}&dismissOnTouch=true'
+                f'&title={playlist["name"]} updated&text={new_songs} track(s) added by {", ".join(added_by)}&dismissOnTouch=true' + \
+                f'&icon=https%3A%2F%2Fwww.iconsdb.com%2Ficons%2Fdownload%2Fwhite%2Fspotify-64.png'
             requests.get(url)
 
     if not dry:
