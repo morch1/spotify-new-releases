7b8f696d30985b6ecf715066ecf4ba4e6d0b2d7a
morch <70644384+morch1@users.noreply.github.com>
2020-10-01 00:23:34 +0200

    update_on_repeat -> update_playlist

--- commands/update_playlist.py
+++ commands/update_playlist.py
@@ -2,7 +2,7 @@ import progressbar
 import time
 
 
-def run(config, playlist_id, on_repeat_id):
+def run(config, playlist_id, src_playlist_id, liked_only=False):
     sp = config.spotify.sp
 
     print('getting playlisted tracks...')
@@ -13,25 +13,35 @@ def run(config, playlist_id, on_repeat_id):
             playlisted_tracks.append(song['track']['id'])
         songs = sp.next(songs) if songs['next'] else None
 
-    print('getting tracks from On Repeat...')
-    onrepeat_tracks = []
-    songs = sp.playlist(on_repeat_id)['tracks']
+    if liked_only:
+        print('getting saved tracks...')
+        liked_tracks = set()
+        songs = sp.current_user_saved_tracks()
+        while songs:
+            for song in songs['items']:
+                liked_tracks.add((song['track']['artists'][0]['id'], song['track']['name']))
+            songs = sp.next(songs) if songs['next'] else None
+
+    print('getting tracks from source playlist...')
+    src_tracks = []
+    songs = sp.playlist(src_playlist_id)['tracks']
     while songs:
         for song in songs['items']:
-            onrepeat_tracks.append(song['track']['id'])
+            if not liked_only or (song['track']['artists'][0]['id'], song['track']['name']) in liked_tracks:
+                src_tracks.append(song['track']['id'])
         songs = sp.next(songs) if songs['next'] else None
 
     print('updating playlist...')
     n_added = 0
     n_moved = 0
-    bar = progressbar.ProgressBar(maxval=len(onrepeat_tracks))
+    bar = progressbar.ProgressBar(maxval=len(src_tracks))
     bar.start()
-    for i, track in enumerate(onrepeat_tracks):
+    for i, track in enumerate(src_tracks):
         bar.update(i)
         try:
             j = playlisted_tracks.index(track)
             if j != i:
-                time.sleep(1)
+                time.sleep(0.1)
                 if not config.dry:
                     sp.playlist_reorder_items(playlist_id, j, i)
                 playlisted_tracks.insert(i, playlisted_tracks.pop(j))
