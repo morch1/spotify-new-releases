e05664978806d98a41f02bec4e748c6fa339e167
morch <70644384+morch1@users.noreply.github.com>
2020-10-10 21:25:54 +0200

    oops

--- config/__init__.py
+++ config/__init__.py
@@ -1,19 +1,19 @@
-class Config:
-    TABLE_KV = 'kv'
+_TABLE_KV = 'kv'
 
+class Config:
     def __init__(self, db, spotify, lastfm, join):
         self.db = db
         self.spotify = spotify
         self.lastfm = lastfm
         self.join = join
         c = db.cursor()
-        c.execute(f'CREATE TABLE IF NOT EXISTS {self.TABLE_KV} (k TEXT, v TEXT, PRIMARY KEY(k))')
+        c.execute(f'CREATE TABLE IF NOT EXISTS {_TABLE_KV} (k TEXT, v TEXT, PRIMARY KEY(k))')
 
     def set_kv(self, k, v):
         c = self.db.cursor()
-        c.execute(f'REPLACE INTO {self.TABLE_KV} (k, v) values (?, ?)', (k, v))
+        c.execute(f'REPLACE INTO {_TABLE_KV} (k, v) values (?, ?)', (k, v))
     
     def get_kv(self, k, default=None):
         c = self.db.cursor()
-        c.execute(f'SELECT v FROM {self.TABLE_KV} WHERE k = ?', (k,))
+        c.execute(f'SELECT v FROM {_TABLE_KV} WHERE k = ?', (k,))
         return (c.fetchone() or (default,))[0]
